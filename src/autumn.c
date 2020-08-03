7327ac2437e544c5aa1452f2b9ff1e654371bfb8
leijin <leijin@111.com.cn>
2020-08-03 16:50:42 +0800

    调整

--- src/autumn.c
+++ src/autumn.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
+#include <string.h>
 #include "maple.h"
 #include "ginkgo.h"
 #include "poplar.h"
@@ -12,11 +13,9 @@
 
 static void autumn_print_version ();
 static void autumn_print_help ();
-
-
+static int autumn_process_command();
 static struct option long_options[] = {
         {"version", no_argument, NULL, 'v'},
-        {"file", required_argument, NULL, 'f'},
         {"run", required_argument, NULL, 'r'},
         {"help", no_argument, NULL, 'h'},
         {NULL, no_argument, NULL, 0},
@@ -24,29 +23,22 @@ static struct option long_options[] = {
 int main(int argc, char **argv) {
     int c;
     int option_index = 0;
-    while ((c = getopt_long(argc, argv, "vrf:h", long_options, &option_index)) != -1) {
+    while ((c = getopt_long(argc, argv, "vrh", long_options, &option_index)) != -1) {
         switch (c) {
             case 'v':
                 // version
                 autumn_print_version();
                 break;
-
-            case 'f':
-                // file
-                printf("%s\n",optarg);
-                m_parser(optarg);
-                g_hello();
-                p_hello();
-                break;
             case 'r':
-                printf("%run\n");
+                printf("run:\n");
                 int flag=1;
                 while (flag) {
                     char str[50];
-                    char *string = str;  //一定要先将指针变量初始化
-                    printf("请输入字符串：");
-                    gets(string);  //也可以写成gets(str);
-                    printf("%s\n", string);  //输出参数是已经定义好的“指针变量名”
+                    char *string = str;
+                    printf("SQL->");
+                    gets(string);
+                    printf("%s\n", string);
+                    flag=autumn_process_command(string);
                 }
                 break;
             case 'h':
@@ -63,6 +55,16 @@ int main(int argc, char **argv) {
     return EXIT_SUCCESS;
 }
 
+int autumn_process_command(char * command){
+    if (strcmp("quit",command)==0){
+        printf("goodbye!\n");
+        return 0;
+    }
+    m_parser(command);
+    g_hello();
+    p_hello();
+    return 1;
+}
 
 void autumn_print_version () {
     printf("autumn %1.1f\n", AUTUMN_VERSION);
@@ -72,6 +74,5 @@ void autumn_print_help () {
     puts("Usage:");
     puts("-v, --version\n\t see version number");
     puts("-r, --run\n\t run ");
-    puts("-f, --file [filename]:\n\t interpret [filename]. Argument not optional.");
     puts("-h, --help:\n\t get this message");
 }
